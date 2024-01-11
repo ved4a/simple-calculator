ae9bccd02816de099813942a5c95b20c545d46c7
ved4a <vedaas.anand@gmail.com>
2024-01-11 17:13:02 +0530

    del: Clear unnecessary comments

--- script.js
+++ script.js
@@ -1,56 +1,5 @@
 "use strict";
 
-// ADDITION FUNCTION
-
-// MULTIPLE ARGUMENT FUNCTIONS
-
-// Addition
-// function sum(...args) {
-//   let total = 0;
-//   for (let i = 0; i < args.length; i++) {
-//     total += args[i];
-//   }
-//   return total;
-// }
-
-// Subtraction
-// function subtract(...args) {
-//   if (args.length === 0) {
-//     return 0;
-//   }
-//   let result = args[0];
-//   for (let i = 1; i < args.length; i++) {
-//     result -= args[i];
-//   }
-//   return result;
-// }
-
-// Multiplication
-// function multiply(...args) {
-//   if (args.length === 0) {
-//     return 0;
-//   }
-//   let result = args[0];
-//   for (let i = 1; i < args.length; i++) {
-//     result *= args[i];
-//   }
-//   return result;
-// }
-
-// Division
-// function divide(...args) {
-//   if (args.length === 0) {
-//     return 0;
-//   }
-//   let result = args[0];
-//   for (let i = 1; i < args.length; i++) {
-//     result /= args[i];
-//   }
-//   return result;
-// }
-
-// ONLY 2 NUMBERS
-
 // Addition
 const sum = (a, b) => a + b;
 
