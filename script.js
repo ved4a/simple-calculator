0f4e459a6317ab3dd143dd07b74c485e2ce4e45c
ved4a <vedaas.anand@gmail.com>
2024-01-11 16:28:44 +0530

    feat: Create addition function with rest parameter

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// Addition Function
+function sum(...args) {
+  let total = 0;
+  for (let i = 0; i < args.length; i++) {
+    total += args[i];
+  }
+  return total;
+}
+
+console.log(sum(2, 3, 4, 1));
