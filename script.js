9d7305501cdaa0df7a353c27e2f894b2cf67460a
ved4a <vedaas.anand@gmail.com>
2024-01-11 17:03:56 +0530

    fix: Change code to ensure only 2 inputs possible

--- script.js
+++ script.js
@@ -1,46 +1,66 @@
 "use strict";
 
-// Addition Function
-function sum(...args) {
-  let total = 0;
-  for (let i = 0; i < args.length; i++) {
-    total += args[i];
-  }
-  return total;
-}
+// ADDITION FUNCTION
 
-// Subtraction Function
-function subtract(...args) {
-  if (args.length === 0) {
-    return 0;
-  }
-  let result = args[0];
-  for (let i = 1; i < args.length; i++) {
-    result -= args[i];
-  }
-  return result;
-}
+// MULTIPLE ARGUMENT FUNCTIONS
 
-// Multiplication Function
-function multiply(...args) {
-  if (args.length === 0) {
-    return 0;
-  }
-  let result = args[0];
-  for (let i = 1; i < args.length; i++) {
-    result *= args[i];
-  }
-  return result;
-}
+// Addition
+// function sum(...args) {
+//   let total = 0;
+//   for (let i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+//   return total;
+// }
+
+// Subtraction
+// function subtract(...args) {
+//   if (args.length === 0) {
+//     return 0;
+//   }
+//   let result = args[0];
+//   for (let i = 1; i < args.length; i++) {
+//     result -= args[i];
+//   }
+//   return result;
+// }
+
+// Multiplication
+// function multiply(...args) {
+//   if (args.length === 0) {
+//     return 0;
+//   }
+//   let result = args[0];
+//   for (let i = 1; i < args.length; i++) {
+//     result *= args[i];
+//   }
+//   return result;
+// }
+
+// Division
+// function divide(...args) {
+//   if (args.length === 0) {
+//     return 0;
+//   }
+//   let result = args[0];
+//   for (let i = 1; i < args.length; i++) {
+//     result /= args[i];
+//   }
+//   return result;
+// }
+
+// ONLY 2 NUMBERS
+
+// Addition
+const sum = (a, b) => a + b;
+
+// Subtraction
+const subtract = (a, b) => a - b;
+
+// Multiplication
+const multiply = (a, b) => a * b;
 
 // Division Function
-function divide(...args) {
-  if (args.length === 0) {
-    return 0;
-  }
-  let result = args[0];
-  for (let i = 1; i < args.length; i++) {
-    result /= args[i];
-  }
-  return result;
-}
+const divide = (a, b) => a / b;
+
+// Operate Function
