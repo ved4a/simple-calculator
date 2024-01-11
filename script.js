375d381bb515b03ecd58d50664b6adb75cc76198
ved4a <vedaas.anand@gmail.com>
2024-01-11 17:11:18 +0530

    feat: Make operate function for base

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
 
 // Operate Function
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return substract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
+}
